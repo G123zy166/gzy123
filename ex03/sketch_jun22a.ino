1029561d05ca23b36d13786f7ddb85c579137894
G123zy166 <1223567538@qq.com>
2026-06-22 19:22:39 +0800

    ex03-v1: 创建目录，搭建SOS millis时序框架

---
+++ ex03/sketch_jun22a.ino
@@ -0,0 +1,33 @@
+const int ledPin = 2;
+unsigned long prevTime = 0;
+unsigned long curTime;
+
+// 时序定义
+const unsigned long shortFlash = 100;
+const unsigned long longFlash = 300;
+const unsigned long gap = 500;
+const unsigned long sosWait = 2000;
+
+int stage = 0;
+bool lightOn = false;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  curTime = millis();
+  if (curTime - prevTime < (lightOn ? (stage%2?shortFlash:longFlash) : gap)) return;
+  prevTime = curTime;
+  lightOn = !lightOn;
+  digitalWrite(ledPin, lightOn);
+
+  if (!lightOn) {
+    stage++;
+    // 完整SOS流程：3短 3长 3短，共18个阶段
+    if (stage >= 18) {
+      stage = 0;
+      delay(sosWait); // 整套结束长停顿
+    }
+  }
+}
